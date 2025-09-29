02f834e2ea441b646aaba511d09e44bf1ab136d2
vrajarshiraj.shah <vrajarshiraj.shah@sikka.ai>
2025-09-29 12:07:12 +0530

    additional string function code

---
+++ Chapter 3/03_strings_functions.py
@@ -0,0 +1,6 @@
+tr="Mahavatar Narsimha"
+print(len(tr)) #print length of string
+print(tr.endswith("simha")) #ends with function
+print(tr.startswith("Maha")) #starts with function
+print(tr.capitalize()) #does capital of string
+
