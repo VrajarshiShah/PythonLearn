e7d41dfa44ef10812b071fb58eee4e848b8aa122
vrajarshiraj.shah <vrajarshiraj.shah@sikka.ai>
2025-09-29 09:53:41 +0530

    New changes

---
+++ Chapter 3/01_intro_to_strings.py
@@ -0,0 +1,6 @@
+name="harry"
+
+nameshort=name[0:3] #start from index 0 till 3 (Exluding 3rd index)
+character1=name[1]
+print(character1)
+print(nameshort)
---
+++ Chapter 3/02_negative_slicing.py
@@ -0,0 +1,9 @@
+name="Harry"
+
+print(name[0:3])
+
+print(name[-4:-1])  # Negative slicing
+print(name[1 :4])
+
+print(name[:4])
+
