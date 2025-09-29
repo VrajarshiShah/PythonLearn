e77a87e878222f8ec604fd5f7cf8109aae4423a3
vrajarshiraj.shah <vrajarshiraj.shah@sikka.ai>
2025-09-29 12:27:16 +0530

    chapter 3 finished

---
+++ Chapter 3/04_escape_seq.py
@@ -0,0 +1,3 @@
+a="HArry is a good boy \n but not bad\t \"boy\""
+print(a)
+
---
+++ Chapter 3/practice_set2.py
@@ -0,0 +1,20 @@
+
+from datetime import date
+today=date.today()
+
+
+name="Vrajarshi"
+letter=f'''
+Dear {name}
+you are selected
+{today}
+'''
+
+print(letter)
+
+
+test="Manoj  and  Chirag is good  QA"
+print(test.replace("  "," "))
+
+paragraph="Dear harry,\n\t This Python cours is nice.\nThanks"
+print(paragraph)
